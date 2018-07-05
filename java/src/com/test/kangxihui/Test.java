5587a6476e94ce9b036451ad938c4a3267ae1be8
kseal <1043326393@qq.com>
2018-07-05 15:45:28 +0800

    555555

---
+++ java/src/com/test/kangxihui/Test.java
@@ -0,0 +1,10 @@
+package com.test.kangxihui;
+
+/**
+ * Created by Administrator on 2018/7/5/005.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
